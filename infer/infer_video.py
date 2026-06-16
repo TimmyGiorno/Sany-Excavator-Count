7315ecf7ad9992eb09bab3cb7fe4c32f315a16e0
ShimizuYoshiKazu <3567815517@qq.com>
2026-06-16 14:01:19 +0800

    卡车重识别算法已弃用，增加了mine相关识别，实现了truck计数

--- infer/infer_video.py
+++ infer/infer_video.py
@@ -24,7 +24,7 @@ print = functools.partial(print, flush=True)
 
 
 class VideoTracker:
-    def __init__(self, video_path, model_path, output_path, tracker_config="bytetrack.yaml"):
+    def __init__(self, video_path, model_path, output_path, siamese_model_path, tracker_config="bytetrack.yaml"):
         """
         初始化视频跟踪器
 
